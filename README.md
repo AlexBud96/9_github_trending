42fd59f24653308195575510b3fec0ac4a463be5
AlexBud96 <AlexSy4ov@gmail.com>
2017-01-11 03:35:43 +0300

    Задача №9 devman

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Github Trends
+# Description
+Script looks for 20 repositories created during the last 7 days with the highest number of stars, print stars quantity, number of open issues and link to repository.
 
-[TODO. There will be project description]
+# How to use
+You should install modules from requirements.txt like that:
+pip install -r requirements.txt
+Compile script and change variables "days_amount" and "top_amount"(if you want to get another repositories):
+python github_trending.py
+days_amount = 7
+top_amount = 20
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,23 @@
+import requests
+from datetime import datetime,timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(days_amount, top_amount):
+    week_ago = datetime.date(datetime.today() - timedelta(days_amount))
+    payload = {'q': 'created:>%s' % week_ago, 'sort': 'stars', 'per_page': top_amount}
+    response = requests.get("https://api.github.com/search/repositories", params=payload)
+    return response.json()["items"]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def print_response(response):
+    for count, repository in enumerate(response, start=1):
+        print('%s) Name of repository: "%s",' % (count, repository['name']),
+              'number of stars: %s, ' % (repository['stargazers_count']),
+              'number of open issues: %s, ' % (repository['open_issues']),
+              'link to repository: %s' % (repository['html_url']))
 
 if __name__ == '__main__':
-    pass
+    days_amount = 7
+    top_amount = 20
+    response = get_trending_repositories(days_amount, top_amount)
+    print_response(response)
